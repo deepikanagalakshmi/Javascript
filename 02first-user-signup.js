f51dc00e2ba970633f8fc704b5b5a8990edabaff
Deepika Nagalakshmi <58824208+deepikanagalakshmi@users.noreply.github.com>
2020-07-18 21:41:56 +0530

    Add files via upload

---
+++ 02first-user-signup.js
@@ -0,0 +1,27 @@
+const id=01;
+var first_name="Deepika";
+var last_name="Nagalakshmi";
+var email="iamdeepikaldc07@gmail.com";
+var pass="abc123";
+var confirm_pass="abc123";
+var course_count=0;
+var is_logged_in_google= false;
+
+//geting input from user
+//full_name=prompt("Enter your name"); //to use it connect to html
+console.log("First name:",first_name);
+console.log("Last name:"+last_name);
+
+console.log(`
+ ID: ${id}
+ NAME:${first_name+last_name}
+ EMAIL:${email}
+ PASSWORD:${pass}
+ CONFIRM PASSWORD:${confirm_pass}
+ COURSE COUNT:${course_count}
+
+
+`);
+
+
+
