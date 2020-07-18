eab63177ea17d5c768180456708ffb702cad672e
Deepika Nagalakshmi <58824208+deepikanagalakshmi@users.noreply.github.com>
2020-07-18 23:32:27 +0530

    typeof - operator precedence
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
    look at this resourse from MDN

---
+++ 04typeof_operator_precedence.js
@@ -0,0 +1,15 @@
+var num1=500;
+var num2=1000;
+
+var result=num1>num2;
+console.log(result); //false
+
+//to find the datatype of any variable, use typeof
+console.log(typeof result); //boolean
+
+//operator precedence resourse:
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
+
+
+
+
