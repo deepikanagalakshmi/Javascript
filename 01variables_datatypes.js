51a8b254c35ebb7b3b13654892cf964bda481190
Deepika Nagalakshmi <58824208+deepikanagalakshmi@users.noreply.github.com>
2020-07-18 21:09:59 +0530

    Create 01variables_datatypes.js

---
+++ 01variables_datatypes.js
@@ -0,0 +1,14 @@
+//variable are reserved space in memory
+//var let const
+
+// full_name -->snake case
+// fullName  -->camel case
+
+var userName="Deepika";
+var loggedIn=false;
+var loginCount=30;
+var paymentMode;
+
+console.log(userName);
+console.log(paymentMode); //undefined
+
