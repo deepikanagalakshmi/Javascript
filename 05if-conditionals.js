31ba4e0441e0e2d17054e9928d362448d7738336
Deepika Nagalakshmi <58824208+deepikanagalakshmi@users.noreply.github.com>
2020-07-19 14:02:26 +0530

    CHECK TEMPERATURE AND PROVIDE MESSAGE

---
+++ 05if-conditionals.js
@@ -0,0 +1,10 @@
+var temperature=30;
+if(temperature<=20){
+    console.log("Its cold outside");
+}
+else if (temperature<= 30){
+    console.log("its normal");
+}
+else{
+    console.log("its too hot");
+}
