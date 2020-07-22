34b7e3a06b365aeeac093c3894d8c4dbbb4a76d5
Deepika Nagalakshmi <58824208+deepikanagalakshmi@users.noreply.github.com>
2020-07-22 12:11:10 +0530

    TERNARY OPERATOR
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators

---
+++ 08ternary-operator.js
@@ -0,0 +1,18 @@
+//show user a signout button if he is authenticated
+//otherwise show his options to ligin
+
+var authenticated=false;
+
+// if(authenticated){
+//     console.log("show signout button");
+// }
+// else{
+//     console.log("show Login options");
+// }
+
+// easy way using ternary operator 
+
+authenticated?console.log("Show signout button"):console.log("show Login options");
+
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators
